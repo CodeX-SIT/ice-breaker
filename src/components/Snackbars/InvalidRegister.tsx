72d1111fa75f526c92685396b3e0e8b933ac2bda
MatricalDefunkt <pramtsmusic@gmail.com>
2024-07-23 19:27:37 +0530

    feat: Update InvalidRegister component to display error message using snackbar

--- src/components/Snackbars/InvalidRegister.tsx
+++ src/components/Snackbars/InvalidRegister.tsx
@@ -1,6 +1,33 @@
-import { Snackbar } from "@mui/material";
 import React from "react";
+import { Alert, Snackbar, Typography } from "@mui/material";
+import Slide, { SlideProps } from "@mui/material/Slide";
+import type { UserCreateResponse } from "@/actions/UserCreateResponse";
 
-export default function InvalidRegister() {
-  return <Snackbar />;
+function SlideTransition(props: SlideProps) {
+  return <Slide {...props} direction="up" />;
+}
+
+export default function InvalidRegister({
+  open,
+  userCreateResponse,
+}: {
+  open: boolean;
+  userCreateResponse: UserCreateResponse;
+}) {
+  return (
+    <Snackbar open={open} TransitionComponent={SlideTransition}>
+      <Alert severity="error" variant="filled" sx={{ width: "100%" }}>
+        <Typography>{userCreateResponse.body.message}</Typography>
+        {userCreateResponse.status === 400 ? (
+          <Typography>
+            {userCreateResponse.body.errors.map((error) => (
+              <Typography key={error.message}>{error.message}</Typography>
+            ))}
+          </Typography>
+        ) : (
+          ""
+        )}
+      </Alert>
+    </Snackbar>
+  );
 }
