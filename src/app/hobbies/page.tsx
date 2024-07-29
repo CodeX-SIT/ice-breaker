abd05f1ee91fa1ff59da08e8cf3242d624c84630
GenDelta <ankush.dutta.kol@gmail.com>
2024-07-29 19:04:54 +0530

    feat: Add hobbySubmit function to handle form submission for user's hobbies

--- src/app/hobbies/page.tsx
+++ src/app/hobbies/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Box,
   Button,
@@ -11,132 +11,144 @@ import {
   styled,
 } from "@mui/material";
 import NavBar from "@/components/NavBar";
+import hobbySubmit, { HobbySubmitReturnType } from "@/actions/hobbySubmit";
+import { useFormState } from "react-dom";
+import InvalidHobbies from "@/components/Snackbars/InvalidHobbies";
+import { useRouter } from "next/navigation";
 
 const HobbiesPage = () => {
-  const Container = styled("div")(({ theme }) => ({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    height: "100vh",
-    backgroundColor: theme.palette.background.default,
-  }));
+  const [state, formAction] = useFormState(hobbySubmit, {
+    error: "defaultError",
+    success: false,
+  } as HobbySubmitReturnType);
+  const [open, setOpen] = React.useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (state.error === "defaultError") return;
+    setOpen(true);
+    setTimeout(() => {
+      setOpen(false);
+    }, 3000);
+    setTimeout(() => {
+      router.push("/");
+    }, 4000);
+  }, [state]);
 
   return (
     <main>
+      <NavBar />
       <section className="flex h-screen w-screen justify-center items-center">
-        <NavBar />
-        <Container>
-          <Box
+        {/* <Container> */}
+        <Box
+          sx={{
+            marginTop: 8,
+            width: "80%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Paper
             sx={{
-              marginTop: 8,
-              width: "80%",
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
-              position: "relative",
-              minHeight: "100vh",
-              paddingTop: "64px",
-              paddingBottom: "64px",
-              overflow: "hidden",
+              overflow: "auto",
+              maxHeight: "80vh",
             }}
           >
-            <Paper
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                position: "relative",
-                overflow: "auto",
-                maxHeight: "calc(100vh - 64px)",
-              }}
+            <Typography component="h1" variant="h5" sx={{ marginTop: 2 }}>
+              Hobbies Page
+            </Typography>
+            <form
+              className="m-4 flex flex-col items-center"
+              action={formAction}
             >
-              <Typography component="h1" variant="h5">
-                Hobbies Page
-              </Typography>
-              <form className="m-4 flex flex-col items-center">
-                <Grid container spacing={2}>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="name"
-                      label="Name"
-                      name="name"
-                      autoComplete="name"
-                      autoFocus
-                      color="primary"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="dateOfBirth"
-                      label="Date of Birth"
-                      name="dateOfBirth"
-                      type="date"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="hobbies"
-                      label="Hobbies"
-                      name="hobbies"
-                      autoComplete="off"
-                      multiline
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="guiltyPleasures"
-                      label="Guilty Pleasures"
-                      name="guiltyPleasures"
-                      autoComplete="off"
-                      multiline
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="favoriteMovies"
-                      label="Favorite Movies"
-                      name="favoriteMovies"
-                      autoComplete="off"
-                      multiline
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="favoriteMusicians"
-                      label="Favorite Musicians"
-                      name="favoriteMusicians"
-                      autoComplete="off"
-                      multiline
-                    />
-                  </Grid>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    name="name"
+                    autoComplete="name"
+                    autoFocus
+                    color="primary"
+                  />
                 </Grid>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2, width: "50%" }}
-                >
-                  <Typography>Submit</Typography>
-                </Button>
-              </form>
-            </Paper>
-          </Box>
-        </Container>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="dateOfBirth"
+                    label="Date of Birth"
+                    name="dateOfBirth"
+                    type="date"
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="hobbies"
+                    label="Hobbies"
+                    name="hobbies"
+                    autoComplete="off"
+                    multiline
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="guiltyPleasures"
+                    label="Guilty Pleasures"
+                    name="guiltyPleasures"
+                    autoComplete="off"
+                    multiline
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="favoriteMovies"
+                    label="Favorite Movies"
+                    name="favoriteMovies"
+                    autoComplete="off"
+                    multiline
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="favoriteMusicians"
+                    label="Favorite Musicians"
+                    name="favoriteMusicians"
+                    autoComplete="off"
+                    multiline
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2, width: "50%" }}
+              >
+                <Typography>Submit</Typography>
+              </Button>
+            </form>
+          </Paper>
+        </Box>
+        <InvalidHobbies open={open} hobbySubmitResponse={state} />
+        {/* </Container> */}
       </section>
     </main>
   );
