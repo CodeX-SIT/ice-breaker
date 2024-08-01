83a06c9e378c843cab3b2f2fcc670fb04a7c2ad0
MatricalDefunkt <pramtsmusic@gmail.com>
2024-08-01 23:59:01 +0530

    chore: Update page component with authentication logic and redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { auth } from "@/auth";
 import NavBar from "@/components/NavBar";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+  if (!session) {
+    redirect("/auth/signin");
+  }
   return <NavBar />;
 }
