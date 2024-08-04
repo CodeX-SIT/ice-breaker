523ec4456698c6454ccd2c617b131317331663bc
pranav-suri <pranavsuri4@gmail.com>
2024-08-05 04:41:27 +0530

    Build Fix: Deleted hobbies and assigned sample daata

--- src/app/api/controllers/sampleData/sampleData.ts
+++ src/app/api/controllers/sampleData/sampleData.ts
@@ -1,11 +1,7 @@
-import generateSampleAssignments from "./assigned";
 import generateSampleUsers from "./users";
-import generateSampleHobbies from "./hobbies";
 import { sequelize } from "@/database/sequelize";
 
 export default async function generateSampleData({ force } = { force: false }) {
   await sequelize.sync({ force });
   await generateSampleUsers();
-  // await generateSampleHobbies();
-  await generateSampleAssignments();
 }
