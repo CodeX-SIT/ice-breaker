6494b996417266364539fb334e46135a7c98e5da
pranav-suri <pranavsuri4@gmail.com>
2024-08-06 21:38:06 +0530

    Gamecode page redirects to aboutme or avatar page as required

--- src/app/gamecode/page.tsx
+++ src/app/gamecode/page.tsx
@@ -3,10 +3,28 @@ import { ADMINS } from "@/constants";
 import { _GameCodePage } from "./GamePage";
 import checkAuthAndRedirect from "@/utils/checkAuthAndRedirect";
 import AdminGameCode from "./AdminGameCode";
+import { AboutUser, Avatar } from "@/database";
+import { redirect } from "next/navigation";
 
 export default async function GameCodePage() {
   const session = await checkAuthAndRedirect();
+  const userId = session.user?.id!;
   if (ADMINS.includes(session.user?.email ?? "")) {
     return <AdminGameCode />;
-  } else return <_GameCodePage />;
+  }
+  // User is not an admin
+  const aboutUser = await AboutUser.findOne({
+    where: {
+      userId,
+    },
+  });
+  if (!aboutUser) redirect("/aboutme");
+  const avatar = await Avatar.findOne({
+    where: {
+      userId,
+    },
+  });
+  if (!avatar) redirect("/aboutme/avatar");
+
+  return <_GameCodePage />;
 }
