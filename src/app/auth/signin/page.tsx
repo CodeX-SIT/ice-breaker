0f2fa514037b765da2070767bd2dbf3b302a43d6
MatricalDefunkt <pramtsmusic@gmail.com>
2024-08-07 00:34:57 +0530

    feat: Refactored authentication page

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,33 +1,11 @@
-import { auth, signIn } from "@/auth";
-import { Box, Button, Paper, Typography } from "@mui/material";
+import { auth } from "@/auth";
 import { redirect, RedirectType } from "next/navigation";
-import GoogleIcon from "@mui/icons-material/Google";
+import SignInPage from "./SignInPage";
 
 export default async function SignIn() {
   const session = await auth();
   if (session) {
     redirect("/", RedirectType.replace);
   }
-
-  return (
-    <main>
-      <section className="flex h-screen w-screen justify-center items-center">
-        <form
-          action={async () => {
-            "use server";
-            await signIn("google");
-          }}
-        >
-          <Box>
-            <Paper>
-              <GoogleIcon sx={{ m: 2, mr: 0 }} />
-              <Button type="submit" variant="contained" sx={{ m: 2 }}>
-                <Typography>Signin with Google</Typography>
-              </Button>
-            </Paper>
-          </Box>
-        </form>
-      </section>
-    </main>
-  );
+  return <SignInPage />;
 }
