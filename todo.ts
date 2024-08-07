53d1e174a56db0145cdcd7ca5ca1c21bc3380675
pranav-suri <pranavsuri4@gmail.com>
2024-08-07 22:17:37 +0530

    todos created

---
+++ todo.ts
@@ -0,0 +1,6 @@
+// TODO: Grid alignment on admin gamecode
+// TODO: Notification delay removal
+// TODO: 4 digit code
+// TODO: Name comparison without case
+// TODO: Error messages for user
+// TODO: Add place to questions
