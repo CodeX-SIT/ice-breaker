242eb02fda715f68438b477ee13c81147b152ea7
MatricalDefunkt <dev@matricaldefunkt.me>
2024-08-08 09:06:16 +0000

    feat: action drawer now redirects

--- src/components/ActionDrawer.tsx
+++ src/components/ActionDrawer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Divider,
   Drawer,
@@ -12,6 +14,7 @@ import AvatarIcon from "@mui/icons-material/AccountCircle";
 import LogOutIcon from "@mui/icons-material/Logout";
 import React from "react";
 import signOutAction from "./signOutAction";
+import { useRouter } from "next/navigation";
 
 function DrawerItem({
   text,
@@ -39,12 +42,25 @@ function ActionDrawer({
   open: boolean;
   toggleDrawer: (open: boolean) => () => void;
 }) {
+  const router = useRouter();
   return (
     <>
       <Drawer open={open} onClose={toggleDrawer(false)}>
         <List disablePadding>
-          <DrawerItem text="Edit Aboutme" icon={<EditIcon />} />
-          <DrawerItem text="Edit Avatar" icon={<AvatarIcon />} />
+          <DrawerItem
+            text="Edit Aboutme"
+            icon={<EditIcon />}
+            onClick={() => {
+              router.push("/aboutme");
+            }}
+          />
+          <DrawerItem
+            text="Edit Avatar"
+            icon={<AvatarIcon />}
+            onClick={() => {
+              router.push("/aboutme/avatar");
+            }}
+          />
           <Divider />
           <DrawerItem
             text="Logout"
